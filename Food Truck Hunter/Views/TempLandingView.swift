b8ad9c1b0431bbc60c79c4c0a5337cb47eee4e99
Preston McCullough <prestonnmc@gmail.com>
2020-10-20 10:05:02 -0700

    User Location Services

--- Food Truck Hunter/Views/TempLandingView.swift
+++ Food Truck Hunter/Views/TempLandingView.swift
@@ -1,19 +1,8 @@
 import SwiftUI
-import MapKit
 
 
 struct LandingView: View {
-    @State private var region = MKCoordinateRegion(
-            center: CLLocationCoordinate2D(
-                latitude: 36.8134,
-                longitude: -119.7461
-            ),
-            span: MKCoordinateSpan(
-                latitudeDelta: 1,
-                longitudeDelta: 1
-            )
-        )
-    
+
     var body: some View {
         TabView {
             Text("Favourites Screen")
@@ -21,11 +10,11 @@ struct LandingView: View {
                     Image(systemName: "heart.fill")
                     Text("Favourites")
             }
-            Map(coordinateRegion: $region)
+            MapView()
                 .tabItem {
                     Image(systemName: "mappin.circle.fill")
                     Text("Nearby")
-            }
+                }.ignoresSafeArea()
             AccountSettingView()
                 .tabItem {
                     Image(systemName: "gear")
