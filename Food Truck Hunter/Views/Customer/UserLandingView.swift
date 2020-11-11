1b9580bd16538d2ad58ba66e6db2e75070440a35
Preston McCullough <prestonnmc@gmail.com>
2020-11-10 19:56:46 -0800

    Annotation description when clicked. As is you must click plus button for annotations to show on screen.

--- Food Truck Hunter/Views/Customer/UserLandingView.swift
+++ Food Truck Hunter/Views/Customer/UserLandingView.swift
@@ -1,7 +1,37 @@
 import SwiftUI
-
+import MapKit
+import FirebaseFirestore
 
 struct LandingView: View {
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    @State private var locations = [MKPointAnnotation]()
+    
+    func getLocations() {
+        Firestore.firestore().collection("Trucks").getDocuments { (snapshot, error) in
+            if let snapshot = snapshot {
+                for document in snapshot.documents {
+                    if let geopoints = document.get("location") {
+                        let point = geopoints as! GeoPoint
+                        print(point)
+                        let lat = point.latitude
+                        let lon = point.longitude
+                        let cllPoint = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+                        let annotation = MKPointAnnotation()
+                        if let truckName = document.get("name"){
+                            let name = truckName as! String
+                            annotation.title = name
+                        }
+                        else {
+                            annotation.title = "Truck"
+                        }
+                        annotation.coordinate = cllPoint
+                        locations.append(annotation)
+                    }
+                }
+            }
+        }
+    }
+    
     var body: some View {
         TabView {
             FavoriteListView()
@@ -9,11 +39,30 @@ struct LandingView: View {
                     Image(systemName: "heart.fill")
                     Text("Favourites")
             }
-            MapView()
+            ZStack{
+                MapView(centerCoordinate: $centerCoordinate, annotations: locations).ignoresSafeArea()
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            getLocations()
+                        }) {
+                            Image(systemName: "plus")
+                        }
+                        .padding()
+                        .background(Color.black.opacity(0.75))
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .clipShape(Circle())
+                        .padding(.trailing)
+                    }
+                }
+            }
                 .tabItem {
                     Image(systemName: "mappin.circle.fill")
                     Text("Nearby")
-                }.ignoresSafeArea()
+                }
             AccountSettingView()
                 .tabItem {
                     Image(systemName: "gear")
--- Food Truck Hunter/Views/MapView.swift
+++ Food Truck Hunter/Views/MapView.swift
@@ -8,49 +8,105 @@
 import SwiftUI
 import MapKit
 import FirebaseFirestore
+
 struct MapView: UIViewRepresentable {
-  var locationManager = CLLocationManager()
-  func setupManager() {
-    locationManager.desiredAccuracy = kCLLocationAccuracyBest
-    locationManager.requestWhenInUseAuthorization()
-    locationManager.requestAlwaysAuthorization()
-  }
-  
-  func makeUIView(context: Context) -> MKMapView {
-    setupManager()
-    let mapView = MKMapView(frame: UIScreen.main.bounds)
-    mapView.showsUserLocation = true
-    mapView.userTrackingMode = .follow
-    Firestore.firestore().collection("Trucks").getDocuments { (snapshot, error) in
-        if let snapshot = snapshot {
-            for document in snapshot.documents {
-                if let geopoints = document.get("location") {
-                    let point = geopoints as! GeoPoint
-                    let lat = point.latitude
-                    let lon = point.longitude
-                    let cllPoint = CLLocationCoordinate2D(latitude: lat, longitude: lon)
-                    
-                    let annotation = MKPointAnnotation()
-                    if let truckName = document.get("name"){
-                        let name = truckName as! String
-                        annotation.title = name
-                    }
-                    else {
-                        annotation.title = "Truck"
-                    }
-                    annotation.coordinate = cllPoint
-                    mapView.addAnnotation(annotation)
-                }
-                                
-                        
-                    
+    @Binding var centerCoordinate: CLLocationCoordinate2D
+    var annotations: [MKPointAnnotation]
+//    var locationManager = CLLocationManager()
+    
+//    func setupManager() {
+//        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+//        locationManager.requestWhenInUseAuthorization()
+//        locationManager.requestAlwaysAuthorization()
+//    }
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        return mapView
+    }
+
+    func updateUIView(_ view: MKMapView, context: Context) {
+        if annotations.count != view.annotations.count {
+                view.removeAnnotations(view.annotations)
+                view.addAnnotations(annotations)
             }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+        
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            parent.centerCoordinate = mapView.centerCoordinate
+        }
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            // this is our unique identifier for view reuse
+               let identifier = "Placemark"
+
+               // attempt to find a cell we can recycle
+               var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+
+               if annotationView == nil {
+                   // we didn't find one; make a new one
+                   annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+
+                   // allow this to show pop up information
+                   annotationView?.canShowCallout = true
+
+                   // attach an information button to the view
+                   annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+               } else {
+                   // we have a view to reuse, so give it the new annotation
+                   annotationView?.annotation = annotation
+               }
+
+               // whether it's a new view or a recycled one, send it back
+               return annotationView
         }
     }
-    return mapView
-  }
-  
-  func updateUIView(_ uiView: MKMapView, context: Context) {
-  }
-    
 }
+
+
+
+
+// struct MapView: UIViewRepresentable {
+//    var locationManager = CLLocationManager()
+//    var annotations = [MKPointAnnotation] ()
+//    func setupManager() {
+//        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+//        locationManager.requestWhenInUseAuthorization()
+//        locationManager.requestAlwaysAuthorization()
+//    }
+//
+//    func makeUIView(context: Context) -> MKMapView {
+//        setupManager()
+//        let mapView = MKMapView(frame: UIScreen.main.bounds)
+//        mapView.showsUserLocation = true
+//        mapView.userTrackingMode = .follow
+//        return mapView
+//    }
+//
+//    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView)
+//        {
+//            if let annotationTitle = view.annotation?.title
+//            {
+//                print("User tapped on annotation with title: \(annotationTitle!)")
+//            }
+//        }
+//        
+//    func updateUIView(_ uiView: MKMapView, context: Context) {
+//        print("Update")
+//        uiView.removeAnnotations(uiView.annotations)
+//        
+//      }
+//    
+//}
