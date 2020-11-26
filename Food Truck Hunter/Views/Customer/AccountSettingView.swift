c6c18cc2a7f00846e66a6158308d915431e200a0
Sue Vang <vangs07@mail.fresnostate.edu>
2020-11-26 08:32:04 -0800

    fix view layout

--- Food Truck Hunter/Views/Customer/AccountSettingView.swift
+++ Food Truck Hunter/Views/Customer/AccountSettingView.swift
@@ -23,62 +23,61 @@ struct AccountSettingView: View {
     
     var body: some View {
         NavigationView{
-        VStack(alignment:.center) {
-            if authState.session != nil {
-                Button(action: refresh){
-                    HStack(spacing:5){
-                        Image(systemName: "arrow.clockwise")
-                            .foregroundColor(.red)
-                        Text("refresh")
-                            .foregroundColor(.red)
-                    }
-                }
-                
-                if (authState.session?.profile_img == ""){
-                    Image("blank-profile-pic")
-                }
-                else{
-                Image(authState.session?.profile_img ?? "blank-profile-pic" )
-                }
-                
-                HStack{
-                    Spacer()
-                        .frame(width:5)
-                    Text("Name:")
-                        .bold()
-                    
-                    if(first == "initial" ){ // if values are still the same from inital query
-                    Text("\(authState.session?.first_name?.capitalized ?? "no first name" ) \(authState.session?.last_name?.capitalized ?? "no last name")") // show from database
-                    }else{
-                        Text("\(first.capitalized) \(last.capitalized)") // show from the state variables (needs to hit refresh button)
+            VStack(alignment:.center) {
+                if authState.session != nil {
+                    Button(action: refresh){
+                        HStack(spacing:5){
+                            Image(systemName: "arrow.clockwise")
+                                .foregroundColor(.red)
+                            Text("refresh")
+                                .foregroundColor(.red)
+                        }
                     }
                     
-                    Spacer()
-                }
-                HStack{
-                    Spacer()
-                        .frame(width:5)
-                    Text("Email:")
-                        .bold()
-                    Text("\(authState.session?.email ?? "no email") ")
-                    Spacer()
-                }
-                HStack(){
-                    Spacer()
-                        .frame(width:5)
-                    Text("Phone Number:")
-                        .bold()
-                    
-                    if(num == "initial" ){
-                        Text("\(authState.session?.phone_number ?? "no number saved") ")
-                    }else{
-                        Text("\(num)")
+                    if (authState.session?.profile_img == ""){
+                        Image("blank-profile-pic")
+                    }
+                    else{
+                    Image(authState.session?.profile_img ?? "blank-profile-pic" )
                     }
                     
-                    
-                    Spacer()
-                }
-                VStack{
+                    HStack{
+                        Spacer()
+                            .frame(width:5)
+                        Text("Name:")
+                            .bold()
+                        
+                        if(first == "initial" ){ // if values are still the same from inital query
+                        Text("\(authState.session?.first_name?.capitalized ?? "no first name" ) \(authState.session?.last_name?.capitalized ?? "no last name")") // show from database
+                        }else{
+                            Text("\(first.capitalized) \(last.capitalized)") // show from the state variables (needs to hit refresh button)
+                        }
+                        
+                        Spacer()
+                    }
+                    HStack{
+                        Spacer()
+                            .frame(width:5)
+                        Text("Email:")
+                            .bold()
+                        Text("\(authState.session?.email ?? "no email") ")
+                        Spacer()
+                    }
+                    HStack(){
+                        Spacer()
+                            .frame(width:5)
+                        Text("Phone Number:")
+                            .bold()
+                        
+                        if(num == "initial" ){
+                            Text("\(authState.session?.phone_number ?? "no number saved") ")
+                        }else{
+                            Text("\(num)")
+                        }
+                        
+                        
+                        Spacer()
+                    }
                     HStack{
                         NavigationLink(destination: CustomerEditProfileView()) {
                             Text("Edit Profile Info")
@@ -95,18 +94,14 @@ struct AccountSettingView: View {
                                 .padding(.trailing,53)
                         
                         }
-                        Spacer()
-                    }.frame(height:20)
-                    
+                    }
                     DefaultButton(label: "Logout", function: signOut, lwidth: 2)
-                    
+                        
                     if successfullySignOut {
                         SignInView()
                     }
-            
                 }
             }
         }
     }
-}
 }
